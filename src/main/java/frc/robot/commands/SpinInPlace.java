24d19d5bda2a2c94f9ee1f1c2f2d3adb41d29a73
frc-3397-robolions <robolions3397@gmail.com>
2024-02-01 19:10:32 -0600

    Kinda working

--- src/main/java/frc/robot/commands/SpinInPlace.java
+++ src/main/java/frc/robot/commands/SpinInPlace.java
@@ -4,26 +4,49 @@
 
 package frc.robot.commands;
 
+import java.util.TimerTask;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Drivetrain;
 
 public class SpinInPlace extends Command {
   /** Creates a new SpinInPlace. */
+  private PIDController m_pidController = new PIDController(0.1, 0, 0);
+  private final Drivetrain m_drivetrain;
+  private double m_startAngle;
+  private Timer starttime = new Timer();
+
   public SpinInPlace(Drivetrain drivetrain) {
     // Use addRequirements() here to declare subsystem dependencies.
-    drivetrain.drive(0, 0, 0.5, false, false);
-    Thread.sleep(1000);
-    drivetrain.drive(0, 0, 0, false, false);
+    addRequirements(drivetrain);
+    m_drivetrain = drivetrain;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_startAngle = m_drivetrain.getGyro().getDegrees();
+
+    starttime.start();
+    m_drivetrain.drive(0, 0, 0.5, false, false);
+    // starttime.schedule(new TimerTask() {
+    // @Override
+    // public void run() {
+    // // Put the code that stops the robot here
+
+    // }
+    // }, /*/ put delay here in milliseconds /*/ 1000);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    if (starttime.get() > 1) {
+      double output = m_pidController.calculate(m_drivetrain.getGyro().getDegrees(), m_startAngle) * 0.5;
+      m_drivetrain.drive(0, 0, output, false, false);
+    }
   }
 
   // Called once the command ends or is interrupted.
